0411a6a63c6e180e3495bd385acbd140185ce488
Emil Tveden Bjerglund <emilbp@gmail.com>
2022-12-06 08:37:42 +0100

    Update advent06.py

--- advent06.py
+++ advent06.py
@@ -1,6 +1,3 @@
-with open("inputs/input06.txt", "r") as f:
-    data = f.read()
-
 def slider(obj, w):
     window = obj[:w]
     yield window
@@ -10,28 +7,23 @@ def slider(obj, w):
         window += s
         yield window
 
+def find_code(data, width):
+    objslider = slider(data, width)
+
+    for i, code in enumerate(objslider):
+        if len(set(code)) == width:
+            print(i, code)
+            endpos = i + width
+            break
+    return endpos
+
+with open("inputs/input06.txt", "r") as f:
+    data = f.read()
+
 # Part 1
-width = 4
-objslider = slider(data, width)
-
-for i, code in enumerate(objslider):
-    if len(set(code)) == 4:
-        print(i, code)
-        startpos = i
-        endpos = i + width
-        break
-
-print("First marker:", endpos)
+first_marker = find_code(data, 4)
+print("First marker:", first_marker)
 
 # Part 2
-width = 14
-objslider = slider(data, width)
-
-for i, code in enumerate(objslider):
-    if len(set(code)) == width:
-        print(i, code)
-        startpos = i
-        endpos = i + width
-        break
-
-print("First message:", endpos)
+first_message = find_code(data, 14)
+print("First message:", first_message)

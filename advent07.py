b45bc667a493e533d431e96d1c1a34e3d49cecb2
Emil Tveden Bjerglund <emilbp@gmail.com>
2022-12-07 18:52:23 +0100

    Part 2 working

--- advent07.py
+++ advent07.py
@@ -1,12 +1,9 @@
-from itertools import zip_longest
-
-
 with open("inputs/input07.txt", "r") as f:
     data = f.readlines()
 
 terminal = [s.strip() for s in data]
 
-
+# Part 1
 def parse_command(l):
     global path
     if l.startswith("cd"):
@@ -36,7 +33,6 @@ for l in terminal:
 
 possible_paths = ['/']
 for p in [c[0].split('/') for c in contents]:
-    print(p)
     paths = []
     path = ''
     for d in p:
@@ -50,6 +46,19 @@ folders = {p for p in possible_paths for p in p}
 folder_sizes = []
 for c in folders:
     folder_sizes.append([c, sum([x[1] for x in contents if x[0].startswith(c)])])
-folder_sizes
+
 sum_of_folders_to_delete = sum([f[1] for f in folder_sizes if f[1] <= 100000])
-sum_of_folders_to_delete
+
+print('Sum of all of the directories with a total size of at most 100000:', sum_of_folders_to_delete)
+
+
+# Part 2
+
+total_size = 70000000
+space_needed = 30000000
+space_used = [f[1] for f in folder_sizes if f[0] == '/'][0]
+need_to_delete = space_needed - (total_size - space_used)
+
+delete_candidates = [f for f in folder_sizes if f[1] > need_to_delete]
+delete_candidates.sort(key = lambda x : x[1])
+print('Smallest directory that, if deleted, would free up enough space on the filesystem to run the update:', delete_candidates[0])

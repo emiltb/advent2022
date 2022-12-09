62e21ba1a0d21924495f65ccf2dfa7012819e7e9
Emil Tveden Bjerglund <emilbp@gmail.com>
2022-12-09 07:52:18 +0100

    Formatting

--- advent08.py
+++ advent08.py
@@ -19,10 +19,10 @@ for (x, y), h in np.ndenumerate(data):
         # We are at an edge
         n_visible += 1
     else:
-        left = np.all(h > data[x, :y])          # Left
-        right = np.all(h > data[x, (y + 1):])  # right
-        above = np.all(h > data[:x, y])         # Above
-        below = np.all(h > data[(x + 1):, y])  # Below
+        left = np.all(h > data[x, :y])  # Left
+        right = np.all(h > data[x, (y + 1) :])  # right
+        above = np.all(h > data[:x, y])  # Above
+        below = np.all(h > data[(x + 1) :, y])  # Below
 
         if np.any([left, right, above, below]):
             n_visible += 1
@@ -49,13 +49,13 @@ def count_trees(vec):
 # are flipped, as the order of elements is important here.
 scenic_score = np.zeros(data.shape)
 for (x, y), h in np.ndenumerate(data):
-    left = np.flip(h > data[x, :y])     # Left
-    right = h > data[x, (y + 1):]      # Right
-    above = np.flip(h > data[:x, y])    # Above
-    below = h > data[(x + 1):, y]      # Below
+    left = np.flip(h > data[x, :y])  # Left
+    right = h > data[x, (y + 1) :]  # Right
+    above = np.flip(h > data[:x, y])  # Above
+    below = h > data[(x + 1) :, y]  # Below
 
     scores = [count_trees(v) for v in [left, right, above, below]]
 
-    scenic_score[x, y] = reduce(lambda x, y: x*y, scores)
+    scenic_score[x, y] = reduce(lambda x, y: x * y, scores)
 
-print('Maximum scenic score in forest:', scenic_score.max())
+print("Maximum scenic score in forest:", scenic_score.max())

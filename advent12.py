1b73628edf0d792bbe360e1e1917b2322b9bf229
Emil Tveden Bjerglund <emilbp@gmail.com>
2022-12-13 22:13:18 +0100

    Working solution for day 12 - needs more work

--- advent12.py
+++ advent12.py
@@ -1,69 +1,118 @@
-from string import ascii_lowercase
-import networkx as nx
-import matplotlib.pyplot as plt
-
-
+# Failed to solve this one.
+# Adapted from https://github.com/matheusstutzel/adventOfCode/blob/main/2022/12/p1.py
 with open("inputs/input12.txt", "r") as f:
     raw_data = f.readlines()
 
-data = [[c for c in s.strip()] for s in raw_data]
-letters = {l: (ord(l) - 96) for l in ascii_lowercase}
-letters["S"] = 1
-letters["E"] = 26
-heights = [[letters[c] for c in s.strip()] for s in raw_data]
+
+def convert_height(h):
+    if h == "S":
+        return "a"
+    if h == "E":
+        return "z"
+    return h
 
 
-pos = {}
-for i, y in enumerate(data):
-    for j, x in enumerate(y):
-        if x == "S":
-            pos["start"] = (i, j)
-        if x == "E":
-            pos["end"] = (i, j)
-print(pos)
+def compare(n1, n2):
+    n1 = ord(convert_height(n1))
+    n2 = ord(convert_height(n2))
+    return n2 <= (n1 + 1)
 
-direction = {"up": [0, -1], "down": [0, 1], "left": [-1, 0], "right": [1, 0]}
 
-fig = plt.figure(figsize=(20 / 2.54, 10 / 2.54), dpi=150)
-ax = plt.axes()
+def dijkstra(start, mat, cost):
+    q = []
+    cost[start] = 0
+    q.append((start, 0))
 
-ax.imshow(heights)
+    while len(q) > 0:
+        n, c = q.pop()  # node, cost
 
-G = nx.Graph()
-x = len(heights[0])
-y = len(heights)
-for i in range(x):
-    for j in range(y):
-        neighbours = {
-            "up": [sum(x) if j > 0 else None for x in zip([i, j], direction["up"])],
-            "down": [
-                sum(x) if j < y - 1 else None for x in zip([i, j], direction["down"])
-            ],
-            "left": [sum(x) if i > 0 else None for x in zip([i, j], direction["left"])],
-            "right": [
-                sum(x) if i < j - 1 else None for x in zip([i, j], direction["right"])
-            ],
-        }
-        for key, item in neighbours.items():
-            if None not in item:
-                height_diff = abs(heights[j][i] - heights[item[1]][item[0]])
+        if cost[n] != c:
+            continue
+        for v in mat[n]:
+            if cost[v] > (c + 1):
+                cost[v] = c + 1
+                q.append((v, c + 1))
 
-                if height_diff <= 1:
-                    ax.plot([i, item[0]], [j, item[1]], color="red", linewidth=1)
 
-                # print(height_diff, f"{i},{j}", f"{item[1]},{item[0]}")
-                if height_diff <= 1:
-                    G.add_edge(f"{i},{j}", f"{item[0]},{item[1]}")
+l = [l.strip() for l in raw_data]
 
-ax.plot(pos["start"][1], pos["start"][0], "g.")
-ax.plot(pos["end"][1], pos["end"][0], "g.")
-print(G)
-astar_path = nx.astar_path(
-    G, f"{pos['start'][1]},{pos['start'][0]}", f"{pos['end'][1]},{pos['end'][0]}"
-)
+lines = len(l)
+cols = len(l[0])
 
-nx.dijkstra_path(
-    G, f"{pos['start'][1]},{pos['start'][0]}", f"{pos['end'][1]},{pos['end'][0]}"
-)
+mat = []
+for i in range(lines):
+    for j in range(cols):
+        pos = j + i * cols
+        v = l[i][j]
+        if v == "S":
+            start = pos
+        if v == "E":
+            end = pos
 
-print(len(astar_path) - 1)
+        adj = []
+        # top
+        if i > 0 and compare(v, l[i - 1][j]):
+            adj.append(j + (i - 1) * cols)
+        # left
+        if j > 0 and compare(v, l[i][j - 1]):
+            adj.append(j - 1 + i * cols)
+        # down
+        if i + 1 < lines and compare(v, l[i + 1][j]):
+            adj.append(j + (i + 1) * cols)
+        # right
+        if j + 1 < cols and compare(v, l[i][j + 1]):
+            adj.append(j + 1 + i * cols)
+
+        mat.append(adj)
+
+cost = [999] * (lines * cols)
+dijkstra(start, mat, cost)
+
+print(cost[end])
+
+
+# Part 2
+# Solution adapted from https://github.com/matheusstutzel/adventOfCode/blob/main/2022/12/p2.py
+# since I really struggled with this one.
+
+
+def compare(n1, n2):
+    n1 = ord(convert_height(n1))
+    n2 = ord(convert_height(n2))
+    return n2 >= (n1 - 1)
+
+
+mat = []
+for i in range(lines):
+    for j in range(cols):
+        pos = j + i * cols
+        v = l[i][j]
+        if v == "S":
+            start = pos
+        if v == "E":
+            end = pos
+
+        adj = []
+        # top
+        if i > 0 and compare(v, l[i - 1][j]):
+            adj.append(j + (i - 1) * cols)
+        # left
+        if j > 0 and compare(v, l[i][j - 1]):
+            adj.append(j - 1 + i * cols)
+        # down
+        if i + 1 < lines and compare(v, l[i + 1][j]):
+            adj.append(j + (i + 1) * cols)
+        # right
+        if j + 1 < cols and compare(v, l[i][j + 1]):
+            adj.append(j + 1 + i * cols)
+
+        mat.append(adj)
+
+cost = [999] * (lines * cols)
+dijkstra(end, mat, cost)
+
+# for i in range(lines):
+#    for j in range(cols):
+#        print("%03d%s"%(cost[ j + i*cols], l[i][j]), end=" ")
+#    print("")
+print(min([c for idx, c in enumerate(cost) if l[idx // cols][idx % cols] == "a"]))

afd15e4e17c6fd1ec9fb45c9b9c84f1e682b39cf
Emil Tveden Bjerglund <emilbp@gmail.com>
2022-12-14 21:52:59 +0100

    Modified solution to day 12, that I understand better.

--- advent12.py
+++ advent12.py
@@ -1,118 +1,112 @@
 # Failed to solve this one.
-# Adapted from https://github.com/matheusstutzel/adventOfCode/blob/main/2022/12/p1.py
+# Adapted from https://github.com/hyper-neutrino/advent-of-code/blob/main/2022/day12p1.py
+from collections import deque
+import matplotlib.pyplot as plt
+from copy import deepcopy
+import numpy as np
+
 with open("inputs/input12.txt", "r") as f:
-    raw_data = f.readlines()
+    raw_data = f.read()
 
+grid = [s.strip().splitlines() for s in raw_data]
+grid = [list(x) for x in open("inputs/input12.txt").read().strip().splitlines()]
+for r, row in enumerate(grid):
+    for c, item in enumerate(row):
+        if item == "S":
+            sr = r
+            sc = c
+            grid[r][c] = "a"
+        if item == "E":
+            er = r
+            ec = c
+            grid[r][c] = "z"
 
-def convert_height(h):
-    if h == "S":
-        return "a"
-    if h == "E":
-        return "z"
-    return h
+# Create the queue
+queue = deque()
 
+# Add the starting node as the first item in the queue
+queue.append((0, sr, sc))
 
-def compare(n1, n2):
-    n1 = ord(convert_height(n1))
-    n2 = ord(convert_height(n2))
-    return n2 <= (n1 + 1)
+# Add the starting node as the first visited node
+vis = {(sr, sc)}
+node_map = deepcopy(grid)
+node_map = np.zeros((len(grid), len(grid[0])))
+node_map[sr, sc] = 0
 
+while queue:
+    # Visit the next node
+    d, r, c = queue.popleft()
 
-def dijkstra(start, mat, cost):
-    q = []
-    cost[start] = 0
-    q.append((start, 0))
-
-    while len(q) > 0:
-        n, c = q.pop()  # node, cost
-
-        if cost[n] != c:
+    # Check all the neighbours of the current node
+    for nr, nc in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
+        # Check if it is a valid neighbour
+        if nr < 0 or nc < 0 or nr >= len(grid) or nc >= len(grid[0]):
             continue
-        for v in mat[n]:
-            if cost[v] > (c + 1):
-                cost[v] = c + 1
-                q.append((v, c + 1))
+        # Check if the node has been visited before
+        if (nr, nc) in vis:
+            continue
+        # Check if the height difference is too large
+        if ord(grid[nr][nc]) - ord(grid[r][c]) > 1:
+            continue
+        # Check if it is the final node
+        if nr == er and nc == ec:
+            print(d + 1)
+        # Add to visited nodes
+        vis.add((nr, nc))
+        queue.append((d + 1, nr, nc))
+        node_map[nr, nc] = d + 1
 
-
-l = [l.strip() for l in raw_data]
-
-lines = len(l)
-cols = len(l[0])
-
-mat = []
-for i in range(lines):
-    for j in range(cols):
-        pos = j + i * cols
-        v = l[i][j]
-        if v == "S":
-            start = pos
-        if v == "E":
-            end = pos
-
-        adj = []
-        # top
-        if i > 0 and compare(v, l[i - 1][j]):
-            adj.append(j + (i - 1) * cols)
-        # left
-        if j > 0 and compare(v, l[i][j - 1]):
-            adj.append(j - 1 + i * cols)
-        # down
-        if i + 1 < lines and compare(v, l[i + 1][j]):
-            adj.append(j + (i + 1) * cols)
-        # right
-        if j + 1 < cols and compare(v, l[i][j + 1]):
-            adj.append(j + 1 + i * cols)
-
-        mat.append(adj)
-
-cost = [999] * (lines * cols)
-dijkstra(start, mat, cost)
-
-print(cost[end])
-
+fig = plt.figure()
+ax = plt.axes()
+ax.imshow(node_map)
 
 # Part 2
-# Solution adapted from https://github.com/matheusstutzel/adventOfCode/blob/main/2022/12/p2.py
-# since I really struggled with this one.
 
 
-def compare(n1, n2):
-    n1 = ord(convert_height(n1))
-    n2 = ord(convert_height(n2))
-    return n2 >= (n1 - 1)
+# Create the queue
+queue = deque()
+
+# Add the starting node as the first item in the queue
+queue.append((0, er, ec))
+
+# Add the starting node as the first visited node
+vis = {(er, ec)}
+node_map = deepcopy(grid)
+node_map = np.zeros((len(grid), len(grid[0]))) + 999
+node_map[er, ec] = 0
+
+while queue:
+    # Visit the next node
+    d, r, c = queue.popleft()
+    node_map[r, c] = d
+
+    # Check all the neighbours of the current node
+    for nr, nc in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
+        # Check if it is a valid neighbour
+        if nr < 0 or nc < 0 or nr >= len(grid) or nc >= len(grid[0]):
+            continue
+        # Check if the node has been visited before
+        if (nr, nc) in vis:
+            continue
+        # Check if the height difference is too large - note that this is
+        # different than the first part since we are tracking the path in reverse
+        if ord(grid[nr][nc]) - ord(grid[r][c]) < -1:
+            continue
+        # Add to visited nodes
+        vis.add((nr, nc))
+        queue.append((d + 1, nr, nc))
 
 
-mat = []
-for i in range(lines):
-    for j in range(cols):
-        pos = j + i * cols
-        v = l[i][j]
-        if v == "S":
-            start = pos
-        if v == "E":
-            end = pos
+fig = plt.figure()
+ax = plt.axes()
+ax.imshow(node_map)
 
-        adj = []
-        # top
-        if i > 0 and compare(v, l[i - 1][j]):
-            adj.append(j + (i - 1) * cols)
-        # left
-        if j > 0 and compare(v, l[i][j - 1]):
-            adj.append(j - 1 + i * cols)
-        # down
-        if i + 1 < lines and compare(v, l[i + 1][j]):
-            adj.append(j + (i + 1) * cols)
-        # right
-        if j + 1 < cols and compare(v, l[i][j + 1]):
-            adj.append(j + 1 + i * cols)
+np.where(np.array(grid) == "a")
+a_list = []
+for r, row in enumerate(grid):
+    for c, item in enumerate(row):
+        if grid[r][c] == "a":
+            a_list.append(node_map[r, c])
 
-        mat.append(adj)
 
-cost = [999] * (lines * cols)
-dijkstra(end, mat, cost)
-
-# for i in range(lines):
-#    for j in range(cols):
-#        print("%03d%s"%(cost[ j + i*cols], l[i][j]), end=" ")
-#    print("")
-print(min([c for idx, c in enumerate(cost) if l[idx // cols][idx % cols] == "a"]))
+min(a_list)

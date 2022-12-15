3753f1c4ec7cd9389bfe86358c4c0f965e22dd8f
Emil Tveden Bjerglund <emilbp@gmail.com>
2022-12-15 13:56:03 +0100

    Part 2 not quite right

--- advent14.py
+++ advent14.py
@@ -58,7 +58,7 @@ while path_blocked is False:
     sand_coords.add((sx, sy))
 
 
-print("Number of sand units, before the first one falls forever:", len(sand_coords)-1)
+print("Number of sand units, before the first one falls forever:", len(sand_coords) - 1)
 
 fig = plt.figure(figsize=(20 / 2.53, 20 / 2.54), dpi=150)
 ax = plt.axes()
@@ -81,3 +81,60 @@ ax.scatter(
 #     c="cyan",
 # )
 ax.invert_yaxis()
+
+
+# Part 2
+
+
+src = (500, 0)
+
+sand_coords = set()
+
+bottom_reached = False
+y_limit = max([y for (x, y) in all_cords])
+
+last_path = []
+
+path_blocked = False
+while path_blocked is False:
+    # for _ in range(2000):
+    # Create a new unit of sand
+    sx = src[0]
+    sy = src[1]
+
+    while True:
+        if sy == y_limit + 2:
+            break
+        # Check if the next position below is vacant
+        elif (sx, sy + 1) not in all_cords and (sx, sy + 1) not in sand_coords:
+            sy += 1
+        elif (sx - 1, sy + 1) not in all_cords and (sx - 1, sy + 1) not in sand_coords:
+            sy += 1
+            sx -= 1
+        elif (sx + 1, sy + 1) not in all_cords and (sx + 1, sy + 1) not in sand_coords:
+            sy += 1
+            sx += 1
+        else:
+            break
+
+    if sy == 0:
+        path_blocked = True
+    sand_coords.add((sx, sy))
+
+
+fig = plt.figure(figsize=(20 / 2.53, 20 / 2.54), dpi=150)
+ax = plt.axes()
+
+ax.scatter(src[0], src[1], c="green", s=1, marker="s")
+ax.scatter(sx, sy, c="red", s=1, marker="s")
+ax.scatter([x[0] for x in all_cords], [x[1] for x in all_cords], s=1, marker="s")
+ax.scatter(
+    [x[0] for x in sand_coords],
+    [x[1] for x in sand_coords],
+    s=1,
+    marker="s",
+    c="orange",
+)
+ax.invert_yaxis()
+
+print("Number of sand units, before the source is blocket:", len(sand_coords) - 1)

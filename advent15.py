2af6bf4f00ea4a8dfd983d30f2aa617779c8a4c1
Emil Tveden Bjerglund <emil@tbjerglund.dk>
2022-12-17 14:13:23 +0100

    Working on more efficient solution

--- advent15.py
+++ advent15.py
@@ -1,4 +1,8 @@
 import re
+from functools import reduce
+def overlaps(a,b):
+    # Returns true if a and b overlaps
+    return max([a[0],b[0]]) <= min([a[1],b[1]])
 
 data = [
     [int(n) for n in re.findall("[\d]+", s)]
@@ -20,9 +24,54 @@ for s, b in zip(sensors, beacons):
         min_x, max_x = s[0] - dist_x, s[0] + dist_x
         x_ranges.append([min_x,max_x])
 
-xvals = set()
-for (x1,x2) in x_ranges:
-    for x in range(x1,x2):
-        xvals.add(x)
+#xvals = set()
+#for (x1,x2) in x_ranges:
+#    for x in range(x1,x2):
+#        xvals.add(x)
 
-print(len(xvals))
+#print(len(xvals))
+
+print(overlaps(x_ranges[0],x_ranges[1]))
+def merge_ranges(x1,x2):
+    if type(x1[0]) == list:
+        return merge_ranges(x1[0],x1[1])
+    if type(x2[0]) == list:
+        return merge_ranges(x2[0],x2[1])
+    if overlaps(x1,x2):
+        return [min(x1[0],x2[0]),max(x1[1],x2[1])]
+    else:
+        return [x1,x2]
+
+print(x_ranges[0], x_ranges[1])
+print(merge_ranges(x_ranges[0], x_ranges[1]))
+
+total_range = reduce(merge_ranges, x_ranges)
+print(total_range[1] - total_range[0])
+
+total_range = reduce(merge_ranges, [[0,10],[17,20],[5,15]])
+print(total_range)
+
+def mergeIntervals(intervals):
+    # Sort the array on the basis of start values of intervals.
+    intervals.sort()
+    stack = []
+    # insert first interval into stack
+    stack.append(intervals[0])
+    for i in intervals[1:]:
+    # Check for overlapping interval,
+        # if interval overlap
+        if stack[-1][0] <= i[0] <= stack[-1][-1]:
+            stack[-1][-1] = max(stack[-1][-1], i[-1])
+        else:
+            stack.append(i)
+
+    print("The Merged Intervals are:", end=" ")
+    for i in range(len(stack)):
+        print(stack[i], end=" ")
+
+arr = [[6,8],[1,9],[2,4],[4,7]]
+print(arr)
+print(mergeIntervals(arr))
+arr = [[0,10],[17,20],[5,15]]
+print(arr)
+print(mergeIntervals(arr))

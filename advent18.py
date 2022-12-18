c768d758dd1d14f8d9863d3323a2a812032bb303
Emil Tveden Bjerglund <emilbp@gmail.com>
2022-12-18 21:36:58 +0100

    Depth-first search for part 2, to find the surface area

--- advent18.py
+++ advent18.py
@@ -1,44 +1,66 @@
-voxels = [
-    [int(n) for n in v.strip().split(",")]
-    for v in open("inputs/input18_example.txt").readlines()
-]
+from collections import deque
+
+voxels = {
+    tuple([int(n) for n in v.strip().split(",")])
+    for v in open("inputs/input18.txt").readlines()
+}
+
+dirs = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
 
 s = 0
-air_voxels = []  # For part 2
 # Check all voxels
 for x, y, z in voxels:
     # Examine all immediate neighbours.
-    for t in [
-        [x + 1, y, z],
-        [x - 1, y, z],
-        [x, y + 1, z],
-        [x, y - 1, z],
-        [x, y, z + 1],
-        [x, y, z - 1],
-    ]:
+    for dx, dy, dz in dirs:
         # If the neighbour does not exist it contributes to
         # surface area
-        if t not in voxels:
-            air_voxels.append(t)
+        if (x + dx, y + dy, z + dz) not in voxels:
             s += 1
 print("Surface area of obsidian:", s)
 
 # Part 2
 
-trapped_voxels = 0
-for x, y, z in air_voxels:
-    neighbours = [
-        [x + 1, y, z],
-        [x - 1, y, z],
-        [x, y + 1, z],
-        [x, y - 1, z],
-        [x, y, z + 1],
-        [x, y, z - 1],
-    ]
-    if all([n in voxels for n in neighbours]):
-        trapped_voxels += 1
+# Set outer bounds that we are sure are outside the obsidian
+def minmax(l):
+    return min(l) - 1, max(l) + 1
 
-print(trapped_voxels)
 
-surface_area = s - trapped_voxels
-print(surface_area)
+min_x, max_x = minmax([x for (x, y, z) in voxels])
+min_y, max_y = minmax([y for (x, y, z) in voxels])
+min_z, max_z = minmax([z for (x, y, z) in voxels])
+
+# Inititate in the lowest corner
+faces = 0
+visited = set((min_x, min_y, min_z))
+
+# Set up a queue of points to visit
+q = deque([(min_x, min_y, min_z)])
+
+# While there are still items in the queue
+while q:
+    # Pick the next item
+    x, y, z = q.popleft()
+
+    # Check all neighbours
+    for dx, dy, dz in dirs:
+        # Set coordinates of neighbour
+        nx, ny, nz = k = (x + dx, y + dy, z + dz)
+
+        # If we are outside the outer bounds
+        if not (min_x <= nx <= max_x and min_y <= ny <= max_y and min_z <= nz <= max_z):
+            continue
+
+        # If the neighbour is part of the obsidian
+        if k in voxels:
+            faces += 1
+            continue
+
+        # If we have already visited the neighbour
+        if k in visited:
+            continue
+
+        # Otherwise add the neighbour to the queue and mark it as visited
+        q.append(k)
+        visited.add(k)
+
+print("Outer surface of obsidian:", faces)
